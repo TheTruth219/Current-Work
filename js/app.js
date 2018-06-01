7df91be28bf1fb98d93a826bdd07f8a9a9d6fc40
TheTruth219 <36748331+TheTruth219@users.noreply.github.com>
2018-06-01 14:55:36 -0700

    Fixed IE and Safari bugs.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,25 @@ $(document).ready(function() {
     $('.icon-three').toggleClass('active-three');
   });
 
+  new WOW().init();
+
+  ///////Page Disable while loading////////////////////////////////////////////
+  setTimeout(function() {
+    $("section").removeClass("disable");
+  }, 7000)
 
 
-})
+  $(".fa-chevron-down").hover(function() {
+
+    $(this).removeClass("fa-lg");
+    $(this).addClass("fa-2x");},
+    function() {
+      $(this).removeClass("fa-2x");
+      $(this).addClass("fa-lg");
+
+  });
+
+
+
+
+});
